63d9c8ceebe6bbb25291cb281ec322540eaeffb9
Kay <efivars@icloud.com>
2024-04-01 22:36:52 +0300

    Move message processing to fn

--- src/bot.rs
+++ src/bot.rs
@@ -13,40 +13,42 @@ impl UrlSanitizerBot {
     UrlSanitizerBot { token }
   }
 
+  fn process_message(bot: Bot, message: Message) {
+    // FIXME: do not create sanitizer each time
+    let sanitizer = YouTubeUrlSanitizer::new();
+
+    match message.text() {
+      Some(text) => {
+        let reply_text: String = match to_url(text) {
+          Ok(url) => match sanitizer.sanitize(&url) {
+            Ok(link) => format!("Success: {}", link),
+            Err(e) => match e {
+              url_sanitizer::sanitizer::SanitizeError::NoDomain => {
+                String::from("Error: specify domain!")
+              }
+              url_sanitizer::sanitizer::SanitizeError::UnacceptableDomain => {
+                String::from("Error: Unsupported domain")
+              }
+            },
+          },
+          Err(e) => format!("ParseError: {}", e.to_string()),
+        };
+
+        let _ = bot.send_message(message.chat.id, reply_text);
+      }
+      None => {
+        let _ = bot.send_dice(message.chat.id);
+      }
+    }
+  }
+
   pub async fn run_forever(&self) {
     let bot = Bot::new(&self.token);
+
     println!("Bot started in long-polling mode!");
 
     teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-      // FIXME: do not create sanitizer each time
-      let sanitizer = YouTubeUrlSanitizer::new();
-
-      match msg.text() {
-        Some(text) => {
-          let reply_text: String = match to_url(text) {
-            Ok(url) => match sanitizer.sanitize(&url) {
-              Ok(link) => format!("Success: {}", link),
-              Err(e) => match e {
-                url_sanitizer::sanitizer::SanitizeError::NoDomain => {
-                  String::from("Error: specify domain!")
-                }
-                url_sanitizer::sanitizer::SanitizeError::UnacceptableDomain => {
-                  String::from("Error: Unsupported domain")
-                }
-              },
-            },
-            Err(e) => format!("ParseError: {}", e.to_string()),
-          };
-
-          bot.send_message(msg.chat.id, reply_text).await?;
-        }
-        None => {
-          // TODO: answer "I accept text only?"
-          bot.send_dice(msg.chat.id).await?;
-        }
-      }
-
-      Ok(())
+      Ok(UrlSanitizerBot::process_message(bot, msg))
     })
     .await
   }
